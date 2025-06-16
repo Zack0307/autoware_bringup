8df90da5cd46806fb17ebef930cffa2ed11bb1c1
Zack930307 <box930307@gmail.com>
2025-06-16 17:29:21 +0800

    add imu (no node)

--- autoware_bringup/publishImage.py
+++ autoware_bringup/publishImage.py
@@ -2,7 +2,7 @@ import rclpy
 import os
 import numpy as np
 from std_msgs.msg import String
-from sensor_msgs.msg import Image, PointCloud2
+from sensor_msgs.msg import Image, PointCloud2, Imu
 from visualization_msgs.msg import Marker, MarkerArray
 from rclpy.node import Node
 from autoware_bringup import publish_utils 
@@ -15,12 +15,14 @@ class PublisherImageNode(Node):
             self.cam_pub = self.create_publisher(Image, '/kitti_image', 10)
             self.PCL = self.create_publisher(PointCloud2, '/kitti_pcl', 10)
             self.marker = self.create_publisher(Marker, '/marker_gaze', 10)
+            self.imu = self.create_publisher(Imu, '/kitti_imu', 10)
             self.timer = self.create_timer(0.1, self.publish_data)  # 每 0.1 秒呼叫一次
 
     def publish_data(self):
         publish_utils.publish_image(self.frame, self.cam_pub)
         publish_utils.publish_pcl(self.frame, self.PCL, self.get_clock())
         publish_utils.publish_marker_array(self.marker, self.get_clock())
+        publish_utils.publish_imu(self.frame, self.imu, self.get_clock())
         self.frame += 1
         if self.frame >= publish_utils.data_number:
             self.frame = 0
--- autoware_bringup/publish_utils.py
+++ autoware_bringup/publish_utils.py
@@ -1,33 +1,38 @@
 import cv2 as cv
 import os
-import rclpy
 import numpy as np
+import pandas as pd
+from tf_transformations import quaternion_from_euler
 from std_msgs.msg import Header
 from sensor_msgs_py import point_cloud2 as pcl2
+from sensor_msgs.msg import Imu
 from visualization_msgs.msg import Marker, MarkerArray
 from cv_bridge import CvBridge
 from builtin_interfaces.msg import Duration
 from geometry_msgs.msg import Point
 
-DATA_PATH = 'src/autoware_bringup/image/2011_09_26_drive_0002_sync/2011_09_26/2011_09_26_drive_0002_sync'
+Image_DATA_PATH = 'src/autoware_bringup/image/2011_09_26_drive_0002_sync/2011_09_26/2011_09_26_drive_0002_sync'
+OXT_DATA_PATH = 'src/autoware_bringup/oxts/2011_09_26_drive_0002_sync/2011_09_26/2011_09_26_drive_0002_sync'
 cv_bridge  = CvBridge()
-data_number = len(os.listdir(os.path.join(DATA_PATH, 'image_02/data')))
+data_number = len(os.listdir(os.path.join(Image_DATA_PATH, 'image_02/data')))
+FRAME_ID = 'map'  # 依你的座標框架而定
+IMU_COLUMN_NAMES = ['lat', 'lon', 'alt', 'roll', 'pitch', 'yaw', 'vn', 've', 'vf', 'vl', 'vu', 'ax', 'ay', 'az', 'af', 'al', 'au',
+                    'wx', 'wy', 'wz', 'wf', 'wl', 'wu','posacc', 'velacc', 'navstat', 'numsats', 'posmode', 'velmode', 'orimode']
 
 def publish_image(frame, cam_pub):
-    img = cv.imread(os.path.join(DATA_PATH, 'image_02/data/%010d.png'%frame))
+    img = cv.imread(os.path.join(Image_DATA_PATH, 'image_02/data/%010d.png'%frame))
     cam_pub.publish(cv_bridge.cv2_to_imgmsg(img, encoding='bgr8'))
-    return data_number
 
 def publish_pcl(frame, PCL, clock):
-    pcl = np.fromfile(os.path.join(DATA_PATH, 'velodyne_points/data/%010d.bin' % frame), dtype=np.float32).reshape(-1, 4)
+    pcl = np.fromfile(os.path.join(Image_DATA_PATH, 'velodyne_points/data/%010d.bin' % frame), dtype=np.float32).reshape(-1, 4)
     header = Header()
     header.stamp = clock.now().to_msg()
-    header.frame_id = 'map'
+    header.frame_id = FRAME_ID
     PCL.publish(pcl2.create_cloud_xyz32(header, pcl[:, :3]))
 
 def publish_marker_array(marker_pub, clock):
     marker = Marker()
-    marker.header.frame_id = "map"  # 依你的座標框架而定
+    marker.header.frame_id = FRAME_ID  # 依你的座標框架而定
     marker.header.stamp = clock.now().to_msg()
     marker.ns = "basic_shapes"
     marker.id = 0
@@ -61,6 +66,29 @@ def publish_marker_array(marker_pub, clock):
 
     marker_pub.publish(marker)
 
+def publish_imu(frame, imu_pub, clock):
+    imu = Imu()
+    Imu_data = os.path.join(OXT_DATA_PATH, 'oxts/data/%010d.txt'%frame)
+    df = pd.read_csv(Imu_data, sep=' ', header=None)
+    df.columns = IMU_COLUMN_NAMES
+    imu_row = df.iloc[0]
+    imu.header.stamp = clock.now().to_msg()
+    imu.header.frame_id = FRAME_ID  # 依你的座標框架而定
 
-
-
+    q = quaternion_from_euler(float(imu_row['roll']), float(imu_row['pitch']), float(imu_row['yaw']))
+    
+    # IMU數據
+    imu.orientation.x = q[0]
+    imu.orientation.y = q[1]
+    imu.orientation.z = q[2]
+    imu.orientation.w = q[3]
+    
+    imu.angular_velocity.x = float(imu_row['wf'])
+    imu.angular_velocity.y = float(imu_row['wl'])
+    imu.angular_velocity.z = float(imu_row['wu'])
+    
+    imu.linear_acceleration.x = float(imu_row['af'])
+    imu.linear_acceleration.y = float(imu_row['al'])
+    imu.linear_acceleration.z = float(imu_row['au'])
+    
+    imu_pub.publish(imu)

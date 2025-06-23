1e82d6d8a328dfef650c1c59e15247bf50a69ea4
Zack930307 <box930307@gmail.com>
2025-06-23 15:30:48 +0800

    add kitti_utils to caliculate 3d bbox and draw 3d bbox on rviz2

--- autoware_bringup/publishImage.py
+++ autoware_bringup/publishImage.py
@@ -1,37 +1,35 @@
 import rclpy
 import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from std_msgs.msg import String
 from sensor_msgs.msg import Image, PointCloud2, Imu, NavSatFix
 from visualization_msgs.msg import Marker, MarkerArray
 from rclpy.node import Node
-from kitti_bringup import publish_utils 
+from autoware_bringup.publish_utils import *
 
 class PublisherImageNode(Node):
     def __init__(self):
-            super().__init__('publisher_data_node')
+            super().__init__('publisher_node')
             self.frame = 0
             self.cam_pub = self.create_publisher(Image, '/kitti_image', 10)
             self.PCL = self.create_publisher(PointCloud2, '/kitti_pcl', 10)
-            self.marker = self.create_publisher(MarkerArray, '/marker_gaze', 10)
+            self.marker = self.create_publisher(Marker, '/marker_gaze', 10)
             self.imu = self.create_publisher(Imu, '/kitti_imu', 10)
             self.gps = self.create_publisher(NavSatFix, '/kitti_gps', 10)
             self.box = self.create_publisher(MarkerArray, '/kitti_3d_box', 10)
             self.timer = self.create_timer(0.1, self.publish_data)  # 每 0.1 秒呼叫一次
 
     def publish_data(self):
-        publish_utils.publish_image(self.frame, self.cam_pub)
-        publish_utils.publish_pcl(self.frame, self.PCL, self.get_clock())
-        publish_utils.publish_marker_array(self.marker, self.get_clock())
-        publish_utils.publish_imu(self.frame, self.imu, self.get_clock())
-        publish_utils.publish_gps(self.frame, self.gps, self.get_clock())
-        publish_utils.publish_3d_box(self.frame, self.box, self.get_clock())
-        self.frame += 1
-        if self.frame >= publish_utils.data_number:
-            self.frame = 0
-        self.get_logger().info(f'Publishing')
+            
+            publish_image(self.frame, self.cam_pub)
+            publish_pcl(self.frame, self.PCL, self.get_clock())
+            publish_marker_array(self.marker, self.get_clock())
+            publish_imu(self.frame, self.imu, self.get_clock())
+            publish_gps(self.frame, self.gps, self.get_clock())
+            publish_3d_box(self.frame, self.box, self.get_clock())
+            self.frame += 1
+            if self.frame >= data_number:
+                self.frame = 0
+            self.get_logger().info(f'Publishing')
 
     
 def main(args=None):
